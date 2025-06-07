e60fe60567e9459970738cae5134abbaab13bcb0
Qasim <mianqasim065@gmail.com>
2025-06-07 23:38:56 +0500

    Create constructor function in TNode.h

---
+++ DLL/TNode.h
@@ -0,0 +1,21 @@
+
+  #include <iostream>
+using namespace std;
+template<class T>
+ class Node
+ {
+ 	private :
+	T info;
+	 Node<T> *next;
+	 public:
+	 Node(T info=0 , Node *next=0)
+	 {
+		this->info=info;
+		this->next=next; 	
+	 }
+	void setinfo(T info);
+	void setnext(Node<T> * next);
+	T getinfo();
+	Node<T>* getnext();
+	void display();
+ };
